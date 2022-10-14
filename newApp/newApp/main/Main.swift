b1b6597c505190c2ad2a4fcf76c9f8d16e79de02
mac <>
2022-10-14 17:08:33 +0900

    지역 기능 추가 커밋 오류실수로 인한 재 커밋

--- newApp/newApp/main/Main.swift
+++ newApp/newApp/main/Main.swift
@@ -6,23 +6,83 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct Main: View {
     @State private var showModal = false
-    @State private var isShowLoading = true
+    @State private var isHideLoading = true
+    @State private var isLocationHideLoading = true
+    @State private var myLocation = ""
+    
+    // 위치 권한 획득
+    var location = LoactionService()
+    
+    // 자기 위치 설정
+    func getLoaction() {
+        location.requestLocation { coordinate in
+            //latitude: 위도, 도: 경도
+            let findLocation = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            let geocoder = CLGeocoder()
+            let locale = Locale(identifier: "Ko-kr")
+            geocoder.reverseGeocodeLocation(findLocation, preferredLocale: locale, completionHandler: {(placemarks, error) in
+                if let address: [CLPlacemark] = placemarks {
+                    if let name: String = address.last?.name {
+                        print((address.last?.description ?? "").components(separatedBy: ", ")[1])
+                        
+                        myLocation = (address.last?.description ?? "").components(separatedBy: ", ")[1]
+                        isLocationHideLoading = true
+                       
+                    }
+                }
+            })
+        }
+    }
+    
+    
+    init() {
+        
+    }
+    
     var body: some View {
         NavigationView{
             ZStack {
                 VStack {
-                    ProgressView().hidden(isShowLoading)
+                    ProgressView().hidden(isHideLoading)
                 }
-                
                 VStack {
+                    Spacer()
+                    HStack {
+                        Text("현재 지역")
+                        Button {
+                            isLocationHideLoading = false
+                            getLoaction()
+                        } label: {
+                            Image(systemName: "globe.central.south.asia.fill")
+                        }
+
+                    }
+                    ZStack {
+                        ProgressView().hidden(isLocationHideLoading)
+                        Text(myLocation).padding(.bottom, 20)
+                            .padding(.top, 5)
+                    }
+                    
+                    Text("선호하는 음식")
+                    HStack {
+                        Text("면")
+                        Text("매운음식")
+                    }.padding(.bottom, 20)
+                        .padding(.top, 5)
+                    Text("싫어하는 음식")
+                    HStack {
+                        Text("마라탕")
+                    }.padding(.bottom, 20)
+                        .padding(.top, 5)
+                    Spacer()
                     Button {
-            //                self.showModal = true
-                        isShowLoading = !isShowLoading
-                        print("show madal")
-                    } label: {
+//                            self.showModal = true
+                        isHideLoading = !isHideLoading
+                                            } label: {
                         Text("선택 !!")
                     }.sheet(isPresented: self.$showModal) {
                         ResultModal()
